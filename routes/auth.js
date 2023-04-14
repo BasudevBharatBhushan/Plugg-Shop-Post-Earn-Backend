9ad0d8c4b977f5a9ad35d636517b0f88bc30e27f
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-04-15 01:32:01 +0530

    test4

--- routes/auth.js
+++ routes/auth.js
@@ -26,46 +26,45 @@ router.get("/instagram", (req, res) => {
 router.get("/instagram/callback", async (req, res) => {
   const code = req.query.code.replace("#_", ""); // remove #_ from code
   try {
-    const str = "Hello, world! callback";
     res.send(code);
     // Exchange authorization code for access token
-    // const tokenParams = {
-    //   client_id: CLIENT_ID,
-    //   client_secret: CLIENT_SECRET,
-    //   grant_type: "authorization_code",
-    //   redirect_uri: REDIRECT_URI,
-    //   code: code,
-    // };
-    // const tokenResponse = await axios.post(
-    //   TOKEN_URL,
-    //   querystring.stringify(tokenParams)
-    // );
-    // const access_token = tokenResponse.data.access_token;
+    const tokenParams = {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      grant_type: "authorization_code",
+      redirect_uri: REDIRECT_URI,
+      code: code,
+    };
+    const tokenResponse = await axios.post(
+      TOKEN_URL,
+      querystring.stringify(tokenParams)
+    );
+    const access_token = tokenResponse.data.access_token;
 
-    // // Retrieve user information
-    // const meUrl = `https://graph.instagram.com/me?fields=id,username&access_token=${access_token}`;
-    // const meResponse = await axios.get(meUrl);
-    // const user_data = meResponse.data;
-    // const user_id = user_data.id;
-    // const username = user_data.username;
+    // Retrieve user information
+    const meUrl = `https://graph.instagram.com/me?fields=id,username&access_token=${access_token}`;
+    const meResponse = await axios.get(meUrl);
+    const user_data = meResponse.data;
+    const user_id = user_data.id;
+    const username = user_data.username;
 
-    // // Store user information in MongoDB
-    // const newUser = new User({
-    //   user_id: user_id,
-    //   username: username,
-    //   access_token: access_token,
-    // });
-    // newUser.save((err, savedUser) => {
-    //   if (err) {
-    //     console.error(err);
-    //     res
-    //       .status(500)
-    //       .send("Error in registering user, please try again later.");
-    //   } else {
-    //     console.log("Saved user:", savedUser);
-    //     res.redirect("/");
-    //   }
-    // });
+    // Store user information in MongoDB
+    const newUser = new User({
+      user_id: user_id,
+      username: username,
+      access_token: access_token,
+    });
+    newUser.save((err, savedUser) => {
+      if (err) {
+        console.error(err);
+        res
+          .status(500)
+          .send("Error in registering user, please try again later.");
+      } else {
+        console.log("Saved user:", savedUser);
+        res.send("User registered successfully!");
+      }
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal server error");
