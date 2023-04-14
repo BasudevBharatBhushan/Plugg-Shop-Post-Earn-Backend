8f0eb1a22b4e6e9b1a08321317b6639701ce324d
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-04-14 11:37:16 +0530

    bug fix

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,28 @@
+const express = require("express");
+const axios = require("axios");
+const User = require("../models/users");
+const querystring = require("querystring");
+
+const router = express.Router();
+
+const CLIENT_ID = "1324208351471430";
+const CLIENT_SECRET = "cb1bbe0a5928e2f22d69abb49017ae37";
+const REDIRECT_URI =
+  "https://plugg-shop-post-earn-backend.onrender.com/auth/instagram/callback";
+const AUTH_URL = "https://api.instagram.com/oauth/authorize";
+const TOKEN_URL = "https://api.instagram.com/oauth/access_token";
+
+router.get("/instagram", (req, res) => {
+  const authParams = {
+    client_id: CLIENT_ID,
+    redirect_uri: REDIRECT_URI,
+    scope: "user_profile,user_media",
+    response_type: "code",
+  };
+  const authUrl = `${AUTH_URL}?${querystring.stringify(authParams)}`;
+  res.redirect(authUrl);
+});
+
 router.get("/instagram/callback", async (req, res) => {
   const code = req.query.code.replace("#_", ""); // remove #_ from code
   try {
@@ -44,3 +69,9 @@ router.get("/instagram/callback", async (req, res) => {
     res.status(500).send("Internal server error");
   }
 });
+
+router.get("/", (req, res) => {
+  res.send("<h1>Hello, world!</h1>");
+});
+
+module.exports = router;
